2c0254991aebf57890af50a8a3f2464d5505411a
jerome hanke (jhk) <jhk@odoo.com>
2020-01-03 11:35:55 +0000

    [FIX] base_vat: new dutch vat number verification
    Steps to reproduce:
    - install contacts and vat number validation
    - setup your company country to "Netherlands"
    - go to contacts > add a company > try to add a dutch vat number
     (NL264077921B03)
    
    Previous behavior:
    Proper vat numbers are considered unvalid and raise a ValidationError
    
    Current behavior:
    Specific check added for dutch vat numbers
    
    Related:
    http://kleineondernemer.nl/index.php/nieuw-btw-identificatienummer-vanaf-1-januari-2020-voor-eenmanszaken
    https://business.gov.nl/regulation/using-checking-vat-numbers/
    http://www.pruefziffernberechnung.de/U/USt-IdNr.shtml
    
    opw-2166380
    
    closes odoo/odoo#43023
    
    X-original-commit: 1dcd03d171ebeb8bed7364e960ad231b745341ff
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>
    Co-authored-by: Nicolas Martinelli <nim@odoo.com>

--- addons/base_vat/base_vat.py
+++ addons/base_vat/base_vat.py
@@ -232,6 +232,10 @@ class res_partner(osv.osv):
                                     r"(?P<ano>[0-9]{2})(?P<mes>[01][0-9])(?P<dia>[0-3][0-9])" \
                                     r"[ \-_]?" \
                                     r"(?P<code>[A-Za-z0-9&\xd1\xf1]{3})$")
+
+    # Netherlands VAT verification
+    __check_vat_nl_re = re.compile("(?:NL)?[0-9A-Z+*]{10}[0-9]{2}")
+
     def check_vat_mx(self, vat):
         ''' Mexican VAT verification
 
@@ -356,6 +360,58 @@ class res_partner(osv.osv):
                 c2 += int(vat[f])
             c2 = c2 % 10
             return int(vat[9]) == c1 and int(vat[10]) == c2
+
+        return False
+
+    def check_vat_nl(self, vat):
+        """
+        Temporary Netherlands VAT validation to support the new format introduced in January 2020,
+        until upstream is fixed.
+
+        Algorithm detail: http://kleineondernemer.nl/index.php/nieuw-btw-identificatienummer-vanaf-1-januari-2020-voor-eenmanszaken
+
+        TODO: remove when fixed upstream
+        """
+
+        try:
+            from stdnum.util import clean
+            from stdnum.nl.bsn import checksum
+        except ImportError:
+            return True
+
+        vat = clean(vat, ' -.').upper().strip()
+
+        if not (len(vat) == 14):
+            return False
+
+        # Check the format
+        match = self.__check_vat_nl_re.match(vat)
+        if not match:
+            return False
+
+        # Match letters to integers
+        char_to_int = {k: str(ord(k) - 55) for k in string.ascii_uppercase}
+        char_to_int['+'] = '36'
+        char_to_int['*'] = '37'
+
+        # Remove the prefix
+        vat = vat[2:]
+
+        # 2 possible checks:
+        # - For natural persons
+        # - For non-natural persons and combinations of natural persons (company)
+
+        # Natural person => mod97 full checksum
+        check_val_natural = '2321'
+        for x in vat:
+            check_val_natural += x if x.isdigit() else char_to_int[x]
+        if int(check_val_natural) % 97 == 1:
+            return True
+
+        # Company => weighted(9->2) mod11 on bsn
+        vat = vat[:-3]
+        if vat.isdigit() and checksum(vat) == 0:
+            return True
 
         return False
 
