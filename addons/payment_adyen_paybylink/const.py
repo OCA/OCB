a72f682c177cbc1f0ad4d0faebd58733b85a5166
Dariusz Kubiak <dariusz@vanmoof.com>
2022-09-01 19:33:30 +0200

    OD-2156 adyen refund endpoint

--- addons/payment_adyen_paybylink/const.py
+++ addons/payment_adyen_paybylink/const.py
@@ -4,4 +4,5 @@
 # See https://docs.adyen.com/api-explorer/#/CheckoutService/v68/overview for Checkout API
 API_ENDPOINT_VERSIONS = {
     '/paymentLinks': 68,          # Checkout API
+    '/payments/{}/refunds': 69,
 }
