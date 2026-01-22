624af90d6f4bb856dcb70114f2c42b3fe49e089e
Jan Suhr <jan@nitrokey.com>
2026-01-22 09:20:46 +0100

    fix trigger to before the meeting start

--- addons/calendar/models/calendar_event.py
+++ addons/calendar/models/calendar_event.py
@@ -1451,7 +1451,7 @@ class Meeting(models.Model):
                         delta = timedelta(hours=duration)
                     elif interval == 'minutes':
                         delta = timedelta(minutes=duration)
-                    trigger.value = delta
+                    trigger.value = -delta
                     valarm.add('DESCRIPTION').value = alarm.name or u'Odoo'
             for attendee in meeting.attendee_ids:
                 attendee_add = event.add('attendee')
